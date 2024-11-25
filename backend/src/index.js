c34497d0d2f4cdb8650dea2aa4dbab5bd477d3e5
bee <bee.devlog@gmail.com>
2024-11-25 23:20:16 +0900

    feat: 라우트가드 middleware 설정 및 토큰실패 케이스

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import http from "http";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -11,7 +12,7 @@ const PORT = process.env.PORT;
 const server = http.createServer(app);
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 server.listen(PORT, () => {
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ message: "인증 실패 - 토큰없음" });
+    }
+  } catch (error) {}
+};
