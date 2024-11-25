a0a5ad68f1623db15d1ed69662000cfb13f60334
bee <bee.devlog@gmail.com>
2024-11-25 23:32:29 +0900

    feat: protectRoute 설정

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -12,5 +12,17 @@ export const protectRoute = async (req, res, next) => {
     if (!decoded) {
       return res.status(401).json({ message: "인증 실패 - 토큰유효하지않음" });
     }
-  } catch (error) {}
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "유저 정보를 찾을 수 없습니다." });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("인증 미들웨어 에러 발생", error.message);
+    res.status(500).json({ message: "서버 에러" });
+  }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,6 +5,7 @@ import {
   signup,
   updateProfile,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -14,5 +15,5 @@ router.post("/login", login);
 
 router.post("/logout", logout);
 
-router.put("/update-profile", updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 export default router;
