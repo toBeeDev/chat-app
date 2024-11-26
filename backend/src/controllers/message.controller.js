1329b1296481b41eaadf20fa1b88060cfb692f5a
bee <bee.devlog@gmail.com>
2024-11-27 00:07:44 +0900

    feat: 메시지 전송 api

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,5 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
@@ -17,6 +19,57 @@ export const getUsersForSidebar = async (req, res) => {
       "사이드바를 위한 유저 데이터 요청에 실패했습니다.",
       error.message
     );
+    res.status(500).json("서버 에러");
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      //내가 보내거나 받은 모든 메시지
+      $or: [
+        {
+          senderId: myId,
+          receiverId: userToChatId,
+        },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("유저 메시지 조회에 실패했습니다.", error.message);
+    res.status(500).json("서버 에러");
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      //이미지 base64로 cloudinary에 업로드
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //TODO: 실시간 반영 w.socket.io
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log("메시지 전송에 실패했습니다.", error.message);
     res.status(500).json("서버 에러");
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,6 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getUsersForSidebar,
   getMessages,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
@@ -10,5 +11,7 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
 
 router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
