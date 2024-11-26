4acc0dc501c7b2f627f0769f38e1ec54435cbf22
bee <bee.devlog@gmail.com>
2024-11-26 23:39:28 +0900

    feat: 사이드바를 위한 유저 목록 호출 api

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,1 +1,22 @@
-export const getUsersForSidebar = async (req, res) => {};
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    //protect route이기에 user_id에 접근가능
+    const loggedInUserId = req.user_id;
+    const filteredUsers = await User.find({
+      _id: {
+        //not equal 로그인 유저
+        $ne: loggedInUserId,
+      }.select("-password"),
+    });
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log(
+      "사이드바를 위한 유저 데이터 요청에 실패했습니다.",
+      error.message
+    );
+    res.status(500).json("서버 에러");
+  }
+};
