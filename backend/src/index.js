6f27e37b595b8e9bda4afa94e4704cf0dfb28be6
bee <bee.devlog@gmail.com>
2024-11-28 00:05:09 +0900

    feat: cors 포트 및 쿠키 설정

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import http from "http";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -14,6 +15,13 @@ const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
