a9d2447ff60a0e0c1f03749e2d37495f5461f1cb
bee <bee.devlog@gmail.com>
2024-12-02 23:04:16 +0900

    feat: 유저 프로필 이미지 수정 기능

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,9 +1,21 @@
 import { Camera, User, Mail } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
+import { useState } from "react";
 
 const ProfilePage = () => {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
-  const handleImageUpload = async (e) => {};
+  const [selectedImg, setSelectedImg] = useState(null);
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async () => {
+      const base64Img = reader.result;
+      setSelectedImg(base64Img);
+      await updateProfile({ profilePic: base64Img });
+    };
+  };
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
@@ -18,7 +30,7 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={authUser.profilePic || "/avatar.png"}
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
                 alt="프로필"
                 className="size-32 rounded-full object-cover border-4 "
               />
