0db7c2e3a854ab10ecf32563be1bae6224871d07
bee <bee.devlog@gmail.com>
2024-12-06 22:37:23 +0900

    feat: useThemeStore 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,11 @@ import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-
+  const { theme } = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -30,7 +31,7 @@ const App = () => {
     );
 
   return (
-    <div data-theme="retro">
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
