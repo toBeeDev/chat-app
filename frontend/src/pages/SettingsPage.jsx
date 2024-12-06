5ce274dd6e64f7e0e67c7bfdcd52e530539abf7a
bee <bee.devlog@gmail.com>
2024-12-06 22:38:53 +0900

    feat: 미리보기 텍스트 설정

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,5 +1,9 @@
+const PREVIEW_MESSAGES = [
+  { id: 1, content: "안녕하세요! 즐거운 하루보내고 계신가요?", isSent: false },
+  { id: 2, content: "난 지금 행복해!", isSent: true },
+];
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return <div></div>;
 };
 
 export default SettingsPage;
