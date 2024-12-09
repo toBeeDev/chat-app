c6cb985988ecc3561fac657f73745673d1a4502c
bee <bee.devlog@gmail.com>
2024-12-09 23:42:55 +0900

    feat: 홈페이지 UI 기초 세팅

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,18 @@ import { useChatStore } from "../store/useChatStore";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
-  return <div>HomePage</div>;
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100dvh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            {/* <Sidebar/> */}
+            {/* {!selectedUser ? <NoChatSelected /> : <ChatContainer />} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
