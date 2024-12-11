d4e3e113a959f0762af9d79f8a370b83d195b7f6
bee <bee.devlog@gmail.com>
2024-12-11 22:59:46 +0900

    feat: ChatContainer 초기 UI 구성

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
 
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) return <div>로딩중...</div>;
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages....</p>
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
