6a945571218b8e53962fa1918282c13455060551
bee <bee.devlog@gmail.com>
2024-12-16 23:11:25 +0900

    feat: MessageInput state&function 설정

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,9 +1,15 @@
 import { useRef, useState } from "react";
+import { useChatStore } from "../store/useChatStore";
 
 const MessageInput = () => {
   const [text, setText] = useState("");
   const [imagePreview, setImagePreview] = useState(null);
   const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+
+  const handleImageChange = (e) => {};
+  const removeImage = () => {};
+  const handleSendMessage = async (e) => {};
 
   return <div>MessageInput</div>;
 };
