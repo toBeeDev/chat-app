7e6ae84742a988ae5a711753d88af086c8dca0ed
bee <bee.devlog@gmail.com>
2024-12-17 21:08:30 +0900

    feat: Message Input UI 제작

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
+import { X } from "lucide-react";
 
 const MessageInput = () => {
   const [text, setText] = useState("");
@@ -15,7 +16,20 @@ const MessageInput = () => {
     <div className="p-4 w-full">
       {imagePreview && (
         <div className="mb-3 flex items-center gap-2">
-          <div className="relative"></div>
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="미리보기"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absoulte -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
         </div>
       )}
     </div>
