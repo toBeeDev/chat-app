6545e6225dabd9426b514c702f0ab3252c1f04d2
bee <bee.devlog@gmail.com>
2024-12-21 02:11:46 +0900

    feat: 이미지제거 함수

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -23,7 +23,10 @@ const MessageInput = () => {
 
     reader.readAsDataURL(file);
   };
-  const removeImage = () => {};
+  const removeImage = () => {
+    setImagePreview(null);
+    if (fileInputRef.current) fileInputRef.current.value = "";
+  };
   const handleSendMessage = async (e) => {};
 
   return (
