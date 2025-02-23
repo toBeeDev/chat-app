c93e8df2fe30997ad20a00d587a5395739e6b31b
bee <bee.devlog@gmail.com>
2025-02-23 22:34:55 +0900

    feat: socket 서버 연결 및 온라인 유저 확인 로직

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import { axios } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -84,10 +85,15 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
-      userId: authUser._id,
+      query: {
+        userId: authUser._id,
+      },
     });
     socket.connect();
     set({ socket: socket });
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
   },
   disconnectSocket: () => {
     if (get().socket?.connected) get().socket.disconnect();
